772a0f6343cd1195dc7f389f284d78ebefc6e5d2
Pavlenko Evgeniy <pavlenkoevgeniy85@gmail.com>
2024-05-06 15:45:33 +0300

    add: console comand for filling up database

---
+++ src/Console/DatabaseFillup.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Console;
+
+use App\Entity\Book;
+use App\Repository\BookRepository;
+use Doctrine\Bundle\DoctrineBundle\Orm\ManagerRegistryAwareEntityManagerProvider;
+use Doctrine\DBAL\DriverManager;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+#[AsCommand(name: 'app:database-fillup', description: 'Fills up database with some data')]
+class DatabaseFillup extends Command
+{
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        return Command::SUCCESS;
+    }
+}
