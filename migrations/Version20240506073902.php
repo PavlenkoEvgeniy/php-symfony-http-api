054abce9e2bf47e4b8a2e14d7d8bf5ce327ab029
Pavlenko Evgeniy <pavlenkoevgeniy85@gmail.com>
2024-05-06 10:47:45 +0300

    add: migration createdAt, updatedAt, deletedAt columns add to tables

---
+++ migrations/Version20240506073902.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20240506073902 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE author ADD createdAt DATETIME NOT NULL, ADD updatedAt DATETIME NOT NULL, ADD deletedAt DATETIME DEFAULT NULL');
+        $this->addSql('ALTER TABLE book ADD createdAt DATETIME NOT NULL, ADD updatedAt DATETIME NOT NULL, ADD deletedAt DATETIME DEFAULT NULL');
+        $this->addSql('ALTER TABLE publisher ADD createdAt DATETIME NOT NULL, ADD updatedAt DATETIME NOT NULL, ADD deletedAt DATETIME DEFAULT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE author DROP createdAt, DROP updatedAt, DROP deletedAt');
+        $this->addSql('ALTER TABLE book DROP createdAt, DROP updatedAt, DROP deletedAt');
+        $this->addSql('ALTER TABLE publisher DROP createdAt, DROP updatedAt, DROP deletedAt');
+    }
+}
