b43b038e476ee69835f2583c9cdb7649ad4617fc
Pavlenko Evgeniy <pavlenkoevgeniy85@gmail.com>
2024-05-10 02:37:49 +0300

    add: console command for deleting all authors without books

--- src/Console/DeleteAuthorsWithoutBooks.php
+++ src/Console/DeleteAuthorsWithoutBooks.php
@@ -3,36 +3,57 @@
 namespace App\Console;
 
 
-use App\Entity\Author;
+
 use App\Repository\AuthorRepository;
-use Doctrine\Bundle\DoctrineBundle\Registry;
-use Doctrine\DBAL\DriverManager;
 use Doctrine\DBAL\Exception;
-use Doctrine\ORM\ORMSetup;
-use Doctrine\ORM\EntityManager;
-use Symfony\Bridge\Doctrine\ManagerRegistry;
+use Doctrine\ORM\EntityRepository;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
 
-
-
 #[AsCommand(name: 'app:delete-authors-without-books')]
 class DeleteAuthorsWithoutBooks extends Command
 {
+    private EntityRepository $entityRepository;
+
+    public function __construct(AuthorRepository $authorRepository)
+    {
+       $this->entityRepository = $authorRepository;
+
+        parent::__construct();
+    }
+
     /**
      * @throws Exception
      */
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-//        $conn = DriverManager::getConnection(['driver' => 'pdo_mysql',
-//                'user' => 'root',
-//                'password' => 'root',
-//                'host' => 'db',
-//                'dbname' => 'symfony-http-api']);
+        $authors = $this->entityRepository->findAll();
 
+        $entityManager = $this->entityRepository->getEntityManager();
+
+
+        foreach ($authors as $author) {
+            $books = $author->getBooks();
+
+            $books_data = [];
+            foreach ($books as $book) {
+                $books_data[] = [
+                    $book->getTitle(),
+                ];
+            }
+
+            if ($books_data === []) {
+                $author->setDeletedAt(new \DateTime('now'));
+                $entityManager->persist($author);
+            }
+
+        }
+        $entityManager->flush();
+
+        echo "Authors without books were deleted successfully! (Soft Delete)" . PHP_EOL;
         return Command::SUCCESS;
     }
 }
